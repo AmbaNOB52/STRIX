7f4e0dfae18ed0c4b8906a7bfd2d9057fcfb95e7
AmbaNOB52 <ambanob52@gmail.com>
2025-02-13 01:13:39 +0000

    Legacy BPF Support
    Signed-off-by: AmbaNOB52 <ambanob52@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1182,40 +1182,30 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
 
+extern bool is_legacy_ebpf;
 
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
-
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-
-
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 	susfs_spoof_uname(&tmp);
 #endif
 #ifdef CONFIG_ANDROID_SPOOF_KERNEL_VERSION_FOR_BPF
-
+    if (!is_legacy_ebpf) {
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4)) {
-
-		strcpy(tmp.release, "6.6.40");
-
+		strcpy(tmp.release, "5.4.186");
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
-
-
-
 #endif
-
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
